ae34ef131ec4a330f7b073ea6f95ccc5fb2f3369
Babu-Bhai-No-1 <rajmohitsaxena@gmail.com>
2025-09-25 19:57:40 +0530

    Use process.env.PORT for Render deployment

--- app.js
+++ app.js
@@ -68,6 +68,6 @@ io.on("connection",function(uniquesocket){
 
 
 
-server.listen(3000,function(){
-    console.log("Listening on port 3000");
+server.listen(process.env.PORT || 3000, () => {
+    console.log("Listening on port " + (process.env.PORT || 3000));
 });
